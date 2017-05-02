ce93af13cc2d5cc5d08469c56ab12d53505abeff
Abhishek Vasu <abhishekvasu@Abhisheks-MacBook-Pro.local>
2017-05-02 03:48:13 -0400

    Cleaned the training data

--- timit/TIMIT/TRAIN/test.py
+++ timit/TIMIT/TRAIN/test.py
@@ -3,6 +3,6 @@ import pandas as pd
 a = pd.read_csv('phn_train.csv')
 b = pd.read_csv('mfcc_train.csv')
 
-print len(b)
-print len(a)
+print a.head(50)
+# print len(a)
 #print len(a.Phoneme.unique())
